17dda31a2f7dc67677eada92168cbc87dffa6595
dobrikov <dobrikov@cs.uni-duesseldorf.de>
2016-08-05 16:40:11 +0200

    An idea how to handle newlines more efficiently.

--- src/main/java/LexHelper.java
+++ src/main/java/LexHelper.java
@@ -1,15 +1,18 @@
 import java.io.*;
 import java.lang.*;
 import java.util.*;
+
 import CSPMparser.lexer.*;
 import CSPMparser.node.TNl;
+import CSPMparser.node.TPlus;
+import CSPMparser.node.Token;
 
 public class LexHelper extends Lexer 
 {
 	// soak multiple new lines
-	private boolean prior_newline = false;
-
-
+	private State helper_state = State.NORMAL;
+	private Token old_token = null;
+    
 	public LexHelper(final PushbackReader in) 
 	{
 		super(in);
@@ -18,18 +21,33 @@ public class LexHelper extends Lexer
 	@Override
 	protected void filter() throws LexerException, IOException 
 	{
+		System.out.println("token: " + token.getText());
 		if (token instanceof TNl) 
 		{
-			if (prior_newline) 
-			{
+			if (helper_state.equals(State.NORMAL)) {
+				// token would be saved and maybe added later
+				helper_state = State.NEWLINE;
+				old_token = token;
 				token = null;
+				System.out.println("newline in NORMAL state");
+			} else if (helper_state.equals(State.NEWLINE)) {
+			    System.out.println("newline in Newline state, it will be ignored");
+			    token = null;  
 			}
-			prior_newline = true;
+		} else {
+			 if (helper_state.equals(State.NEWLINE)) {
+				 // change to normal again
+				 System.out.println("Change to normal state");
+				 helper_state = State.NORMAL;
+				 if (token instanceof TPlus) {
+					 token = null;
+				 }
+			 } else {
+				 
+				 old_token = null;
+			 }
 		}
-		else 
-		{
-			prior_newline = false;
-		}
+
 	}
 
 }
