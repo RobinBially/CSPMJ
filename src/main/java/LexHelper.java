766a71bd6c03e985ca3b9e2154d02b3b9d0e0e08
RobinBia <robin.bially@uni-duesseldorf.de>
2016-08-05 14:30:48 +0200

    Fix something

--- src/main/java/LexHelper.java
+++ src/main/java/LexHelper.java
@@ -1,27 +1,35 @@
-import java.io.IOException;
-import java.io.PushbackReader;
-
+import java.io.*;
+import java.lang.*;
+import java.util.*;
 import CSPMparser.lexer.*;
 import CSPMparser.node.TNl;
 
-public class LexHelper extends Lexer {
+public class LexHelper extends Lexer 
+{
 	// soak multiple new lines
 	private boolean prior_newline = false;
-	
-	public LexHelper(final PushbackReader in) {
+
+
+	public LexHelper(final PushbackReader in) 
+	{
 		super(in);
 	}
 	
 	@Override
-	protected void filter() throws LexerException, IOException {
-		if (token instanceof TNl) {
-			if (prior_newline) {
+	protected void filter() throws LexerException, IOException 
+	{
+		if (token instanceof TNl) 
+		{
+			if (prior_newline) 
+			{
 				token = null;
 			}
 			prior_newline = true;
 		}
-		else {
+		else 
+		{
 			prior_newline = false;
 		}
 	}
+
 }
